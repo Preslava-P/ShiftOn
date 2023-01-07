7940eee7fa442beb45539fbb04273abde9b446d2
Preslava <preslavap20@gamil.com>
2023-01-07 21:03:20 +0200

    create database

---
+++ ShiftOn/Models/Schedule.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShiftOn.Models
+{
+    public class Schedule
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ScheduleId { get; set; }
+
+        [Required]
+        public int ShiftId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public int VacationRequestId { get; set; }
+
+        public Shift Shift { get; set; }
+        public User User { get; set; }
+        public VacationRequest VacationRequest { get; set; }
+    }
+}
---
+++ ShiftOn/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShiftOn.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public Guid ScheduleId { get; set; }
+        [Required]
+        public int VacationRequestId { get; set; }
+
+        public ICollection<Schedule> Schedules { get; set; }
+        public ICollection<VacationRequest> VacationRequests { get; set;}
+    }
+}
