386be3f57c2b89606d73f5b04022cbce7158c51a
Preslava <preslavap20@gamil.com>
2023-01-11 13:26:02 +0200

    ress

---
+++ ShiftOn/Controllers/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShiftOn.Models;
+
+namespace ShiftOn.Controllers
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddAsync(User item);
+        void Create (T item);
+        void Delete(Guid id);
+        void Details (dynamic id);
+        void Edit (T item);
+        Task GetAsync();
+        Task SaveAsync();
+    }
+}
